413572cc14977d93d86274850979031667e63143
estefaniCruz <estefanitovardelacruz@gmail.com>
2026-05-05 23:21:41 +0200

    añadir secciones principales del portfolio (experiencia, educación, habilidades, contacto y footer)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import tailwindcss from '@tailwindcss/vite'
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [svelte()],
+    integrations: [
+        svelte(),
+        icon(),
+    ],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
